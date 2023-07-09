a2c006af70a264be43d9bcb5634a9b12bd2c5ade
smadeveloper <72317793+smadeveloper@users.noreply.github.com>
2023-07-09 11:11:08 +0300

    Create parser05-function-call.py

---
+++ parser05-function-call.py
@@ -0,0 +1,115 @@
+
+class Token(object):
+
+    def __init__(self,atype,avalue):
+        self.type = atype
+        self.value = avalue
+
+    def __eq__(self,other):
+        if isinstance(other,Token):
+            return self.type==other.type and self.value==other.value
+        return self.type==other
+
+    def __repr__(self):
+        return f"({self.type} {self.value})"
+
+def tokenizer(xs):
+    tokens=[]
+    symbols = {",":"coma","=":"equal", ";":"semi", "(":"lparen",")":"rparen","+":"op","-":"op","*":"op","/":"op" }
+    for x in xs:
+        if x.isalpha():
+            tokens.append( Token("name",x) )
+        elif x.isdigit():
+            tokens.append( Token("number", int(x)) )
+        elif x in symbols.keys():
+            tokens.append( Token(symbols[x],x) )
+    return tokens
+
+
+class Parser:
+    def __init__(self,code):
+        self.code = code
+        self.i=0
+        self.current = self.code[0]
+        
+    def next(self):
+        self.i+=1
+        alist = self.code[self.i:self.i+1] or [Token("eof",object())]
+        self.current = alist[0]
+    
+    def matching(self,*args):
+        for i, arg in enumerate(args):
+            if arg==None or arg==self.code[self.i+i]:
+                continue
+            if isinstance(arg,list):
+                if self.code[self.i+i] in arg:
+                    continue
+            return False
+        return True
+    
+    def match(self,a):
+        if self.current==a or (isinstance(a,list) and self.current in a):
+            result=self.current.value
+            self.next()
+            return result
+        raise Exception(f"error matching {a}")
+    
+    def parse_function_call(self):
+        params = []
+        func = self.match("name")
+        self.match("lparen")
+        while True:
+            params.append(self.parse_expression())
+            if self.matching("coma"):
+                self.match("coma")
+                continue
+            
+            if self.matching("rparen"):
+                break
+            raise Exception("ERROR: Syntax error while parsing function call")
+        self.match("rparen")
+        return ["Call",func,params]
+    
+    def parse_expression(self,left=False,op=False):
+        precedence = {"+":0,"-":0,"*":2,"/":1}
+        
+        if self.matching("name","lparen"):
+            result = self.parse_function_call()
+            if left:
+                result = [op,left,result]
+            if self.matching("op"):
+                op2 = self.match("op")
+                return self.parse_expression(result,op2)
+            return result
+        
+        if self.matching("lparen"):
+            self.match("lparen")
+            result=self.parse_expression()
+            self.match("rparen")
+            if left:
+                result = [op,left,result]
+            if self.matching("op"):
+                op2 = self.match("op")
+                return self.parse_expression(result,op2)
+            return result
+            
+        if self.matching(["name","number"], ["semi","rparen","coma"]):
+            name_or_number = self.match(["name","number"])
+            if left:
+                return [op,left,name_or_number]
+            return name_or_number
+        
+        if self.matching(["name","number"],"op") and left:
+            name_or_number = self.match(["name","number"])
+            op2 = self.match("op")
+            if precedence[op]>precedence[op2]:
+                return self.parse_expression([op,left,name_or_number],op2)
+            return [op,left,self.parse_expression(name_or_number,op2)]
+        
+        if self.matching(["name","number"], "op"):
+            name_or_number = self.match(["name","number"])
+            op2 = self.match("op")
+            return self.parse_expression(name_or_number,op2)
+    
+p=Parser(tokenizer("f(g(1,2));"))
+print(p.parse_expression())
